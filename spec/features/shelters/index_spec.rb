d47682f707d08f34e7ba0770f0ccc5033349a528
“Neeru <neeram85@gmail.com>
2020-07-01 20:05:47 -0600

    User story 2 complete

---
+++ spec/features/shelters/index_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Shelter Index page' do
+  it 'When I visit /shelters I see the names of all shelters' do
+    cozy_kitten = Shelter.create(name: "Cozy Kitten Animal Shelter")
+    playful_pups = Shelter.create(name: "Playful Puppies Animal Shelter")
+
+    visit '/shelters'
+
+    expect(page).to have_content(cozy_kitten.name)
+    expect(page).to have_content(playful_pups.name)
+  end
+end
