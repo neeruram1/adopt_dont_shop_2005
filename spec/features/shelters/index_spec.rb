6fd98c3a229374ff82621c8f1d286266221b9bd4
“Neeru <neeram85@gmail.com>
2020-07-04 13:04:59 -0600

    User story 14 complete

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -15,5 +15,14 @@ RSpec.describe 'Shelter Index page' do
       visit '/shelters'
       click_link "Edit #{cozy_kitten.name}'s Info"
       expect(current_path).to eq("/shelters/#{cozy_kitten.id}/edit")
-    end
+  end
+
+  describe "When I visit /shelters I see a link to delete shelter next to every shelter"
+    it "When I click the link the shelter is deleted and I no longer see the page on the shelter index page" do
+      cozy_kitten = Shelter.create(name: "Cozy Kitten Animal Shelter")
+      visit '/shelters'
+      click_link "Delete #{cozy_kitten.name}"
+    expect(current_path).to eq("/shelters")
+    expect(page).to_not have_content("Cozy Kitten Animal Shelter")
+  end
 end
